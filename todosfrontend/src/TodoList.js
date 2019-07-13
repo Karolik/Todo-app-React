8c0b6e8d51c0bba865b8591f6af4a3d8495c5832
Karolina Gauvain <karolina.gauvain@gmail.com>
2019-07-13 16:50:51 +0200

    Load initial data from Todo API

--- todosfrontend/src/TodoList.js
+++ todosfrontend/src/TodoList.js
@@ -1,6 +1,20 @@
 import React, {Component} from 'react';
 
 class TodoList extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			todos: []
+		};
+	}
+	
+	componentWillMount(){
+		fetch('/api/todos')
+		.then(data =>data.json())
+		.then(todos => this.setState({todos}))
+		;
+	}
+	
 	render() {
 		return(
 			<h1>Todo List!</h1>
