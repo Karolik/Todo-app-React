cf58bad2f9cfbaf2267f2dfb6b10798532575fe1
Karolina Gauvain <karolina.gauvain@gmail.com>
2019-07-13 18:52:30 +0200

    Add functionality of adding todos
    Add todos by submitting the Todo form.

--- todosfrontend/src/TodoList.js
+++ todosfrontend/src/TodoList.js
@@ -10,6 +10,7 @@ class TodoList extends Component{
 		this.state = {
 			todos: []
 		};
+	this.addTodo = this.addTodo.bind(this);	
 	}
 	
 	componentWillMount(){
@@ -35,17 +36,46 @@ class TodoList extends Component{
 		.then(todos => this.setState({todos}));
 	}
 	
+	addTodo(val){
+		fetch(APIURL, {
+			method: 'post',
+			headers: new Headers({
+				'Content-Type': "application/json"
+			}),
+			body: JSON.stringify({name: val})
+		})
+		.then(resp => {
+			if(!resp.ok) {
+				if(resp.status >=400 && resp.status <500){
+					return resp.json().then(data => {
+						let err = {errorMessage: data.message};
+						throw err;
+					});
+				} else {
+					let err = {errorMessage: "Please try again later, server is not responding"};
+					throw err;
+				}
+			}
+			return resp.json();
+		})
+		.then(newTodo => {
+			this.setState({todos: [...this.state.todos, newTodo]});
+		});
+	}
+	
 	render() {
-	const todos = this.state.todos.map((t) => (
+		const todos = this.state.todos.map((t) => (
           <TodoItem
           key={t._id}
           {...t}
+          //onDelete={this.deleteTodo.bind(this,t._id)}
+          //onToggle={this.toggleTodo.bind(this,t)}
           />
         ));
 		return (
 		  <div>
 		   <h1>Todo List!</h1>
-		   <TodoForm />
+		   <TodoForm addTodo={this.addTodo}/>
 		   <ul>
 			{todos}
 		   </ul>
